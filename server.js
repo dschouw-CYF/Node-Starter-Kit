e48a8b357c6a9b257904a0043a44fd5ca0a3ecbc
Denver Schouw <dschouw@gmail.com>
2022-11-08 13:07:36 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,17 @@
+// Import/require express and invoke express and store it in a variable called app
+const express = require('express');
+const app = express();
+
+// Add some routes to respond to requests
+// req is the Request object, res is the Response object
+// (these are just variable names, they can be anything but it's a convention to call them req and res)
+app.get("/", function (request, response) {
+    response.send("Hello World!");
+  });
+
+app.get("/chocolate", (request,response) => response.send("chocolate, yay!"));
+app.get("/myname", (request,response) => response.send("My name is Denver"));
+// end the routes
+
+// start the server
+app.listen(3000, () => console.log("The server has started!"));
